0b4b93c4b9e462fafc32be597c6a6ff270506df5
Vikas Kotary <viki57394@GMAIL.COM>
2023-04-22 22:58:22 +0530

    Fetching the details done -> implementing UI

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -2,31 +2,33 @@ import React, { useContext } from "react";
 import { StateContext } from "../context/Context";
 
 const Transactions = ({ transactions }) => {
-  const {address} = useContext(StateContext);
+  const { address } = useContext(StateContext);
   return (
     <div
-    
-    style={{
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      flexDirection: 'column',
-      gap: '15px'
-    }}>
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        gap: "15px",
+      }}
+    >
       {transactions.map((el, i) => {
         return (
-        <div key={i}
-        
-        style={{
-          display: 'flex',
-          justifyContent: 'center', alignItems: 'center',
-          gap: '20px'
-        }}>
-        <span>From: {el.from}</span>  
-        <span>To: {el.to}</span>  
-        <span>{el.from === address ? 'OUT' : 'IN'}</span>  
-        </div>
-        )
+          <div
+            key={i}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              gap: "20px",
+            }}
+          >
+            <span>From: {el.from}</span>
+            <span>To: {el.to}</span>
+            <span>{el.from === address ? "OUT" : "IN"}</span>
+          </div>
+        );
       })}
     </div>
   );
