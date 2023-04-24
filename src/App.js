934b52e5c0aff7491215d3eb3c5a277f763da94c
Vikas Kotary <viki57394@GMAIL.COM>
2023-04-24 06:09:29 +0530

    IFPS hosting

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@
 import React from 'react';
 import Details from "./Routes/Details.jsx"
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom';
 import Hero from './Hero.jsx';
 import Accounts from './Routes/Accounts.jsx';
 
 function App() {
 
   return (
-    <BrowserRouter>
+    <HashRouter basename='/'>
       <Hero />
       <Routes>
-        <Route path='/*' element={<Details />} />
+        <Route path='/details' element={<Details />} />
         <Route path='/' element={<Accounts />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
